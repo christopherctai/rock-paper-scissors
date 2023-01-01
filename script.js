172d252e532bd54a39d8f9bd6e390f55a9395112
Chris Tai <christopherctai@gmail.com>
2022-12-31 22:51:51 -0800

    Create index and script files, code game

---
+++ script.js
@@ -0,0 +1,74 @@
+console.log("Hello World")
+
+console.log(game(playRound));
+
+function getComputerChoice() {
+    let choices = ["Rock", "Paper", "Scissors"];
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "Tie! No one wins this round"
+    } else if ((playerSelection === "Rock" && computerSelection === "Paper") || (playerSelection === "Paper" && computerSelection === "Scissors") || (playerSelection === "Scissors" && computerSelection === "Rock")) {
+        return `You lose! ${computerSelection} beats ${playerSelection}!`
+    } else {
+        return `You win! ${playerSelection} beats ${computerSelection}!`
+    }
+}
+
+
+function game(playRound) {
+    let userWinCount = 0;
+    let computerWinCount = 0;
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Choose your weapon!");
+        let choices = ["Rock", "Paper", "Scissors"];
+        if (!choices.includes(playerSelection.charAt(0).toUpperCase() + playerSelection.toLowerCase().slice(1))) {
+            computerWinCount++;
+            return "That is an invalid choice. Computer gets a point by default.";
+        };
+        playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+        let computerSelection = getComputerChoice();
+        let result = playRound(playerSelection, computerSelection);
+        if (result === `You lose! ${computerSelection} beats ${playerSelection}!`) {
+            computerWinCount++;
+        } else if (result === `You win! ${playerSelection} beats ${computerSelection}!`) {
+            userWinCount++;
+        }
+        console.log(result);
+    }
+    if (userWinCount > computerWinCount) {
+        return "Game over! You won against the computer!";
+    } else if (userWinCount > computerWinCount) {
+        return "Game over! You lost against the computer!";
+    } else {
+        return "Game over! It was a tie!";
+    }
+}
+
+
+/*
+function with no input 
+	define array of choices 
+	randomly choose something from the array of Rock, Paper, or Scissors
+	return Rock, Paper, or Scissors 
+
+function playRound that takes playerSelection and computerSelection as parameters 
+	compare player to computer selection	
+	if playerSelection === computerSelection 
+		return tie 
+	if playerSelection loses
+		return You lose 
+	if playerSelection wins 
+		return you win
+	
+function that takes playRound function to play a 5 round game 
+    get user input 
+    verify the user input 
+	for 5 times 
+		playRound()
+    keep track of the score, defining variables for computer victory and user victory
+
+*/
